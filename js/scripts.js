1ff3c6c141d18858095c07b3427a9da92078333a
Ultra Zacky <ultrazacky@gmail.com>
2022-01-14 10:36:19 -0800

    add information into the branching element for finding what to display, css classes for each programming language: ruby, python and c sharp. commenting out parts of the branching for debugging. code throwing else statement, but not showing if or if else statements in the branch. --Work-In-Progress--

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $("form#selections").submit(function(){
+  $("form#selections").submit(function(event){
 
     const firstAnswer = $("select#numLet").val(); 
     const secondAnswer = $("select#stylFunc").val();
@@ -8,23 +8,37 @@ $(document).ready(function() {
     const fourthAnswer = $("select#runWalk").val();
     const fifthAnswer = $("select#mornEven").val();
 
+    $("userLangType").removeAttr("class");
     if (firstAnswer === "numbers" && secondAnswer === "style" && thirdAnswer === "old" && fourthAnswer === "run" && fifthAnswer === "morning"){
       $(".progLanguage").show();
-      $(".userLangType").append("<p>You got C#!</p>")
-    } else if (){
-      $(".progLanguage").show();
-    } else if (){
-      $(".progLanguage").show();
-    } else if () {
-      $(".progLanguage").show();
-    } else if () {
-      $(".progLanguage").show();
-    } else if () {
-      $(".progLanguage").show();
+      $(".userLangType").addClass("c-sharp");
+      $(".userLangType").append("<p>You got C#!</p>");
+    // } else if (firstAnswer === "numbers" && secondAnswer === "style" && thirdAnswer === "old" && fourthAnswer === "run" && fifthAnswer === "morning"){
+    //   $(".progLanguage").show();
+    //   $(".userLangType").addClass("c-sharp");
+    //   $(".userLangType").append("<p>You got C#!</p>");
+    // } else if (firstAnswer === "numbers" && secondAnswer === "style" && thirdAnswer === "old" && fourthAnswer === "run" && fifthAnswer === "morning"){
+    //   $(".progLanguage").show();
+    //   $(".userLangType").addClass("ruby");
+    //   $(".userLangType").append("<p>You got C#!</p>");
+    // } else if (firstAnswer === "numbers" && secondAnswer === "style" && thirdAnswer === "old" && fourthAnswer === "run" && fifthAnswer === "morning") {
+    //   $(".progLanguage").show();
+    //   $(".userLangType").addClass("ruby");
+    //   $(".userLangType").append("<p>You got C#!</p>");
+    // } else if (firstAnswer === "numbers" && secondAnswer === "style" && thirdAnswer === "old" && fourthAnswer === "run" && fifthAnswer === "morning") {
+    //   $(".progLanguage").show();
+    //   $(".userLangType").addClass("python");
+    //   $(".userLangType").append("<p>You got C#!</p>");
+    // } else if (firstAnswer === "numbers" && secondAnswer === "style" && thirdAnswer === "old" && fourthAnswer === "run" && fifthAnswer === "morning") {
+    //   $(".progLanguage").show();
+    //   $(".userLangType").addClass("python");
+    //   $(".userLangType").append("<p>You got C#!</p>");
     } else {
       $("#invalidEntry").show();
       $(".progLanguage").hide();
     }
+
+    event.preventDefault();
 
   });
 
