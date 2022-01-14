902d6620b7badd809dfdb82cc82590dde6e80be5
Ultra Zacky <ultrazacky@gmail.com>
2022-01-14 15:02:07 -0800

    add nesting to branching to be able to use && operation. code throwing error when using 5 && in a row inside an if.

--- js/scripts.js
+++ js/scripts.js
@@ -8,31 +8,94 @@ $(document).ready(function() {
     const fourthAnswer = $("select#fourthQuestion").val();
     const fifthAnswer = $("select#fifthQuestion").val();
 
-    $("userLangType").removeAttr("class");
     if (firstAnswer === 'select' || secondAnswer === 'select' || thirdAnswer === 'select' || fourthAnswer === 'select' || fifthAnswer === 'select') { 
       $("#invalidEntry").show();
       $(".resultsOne").hide();
       $(".resultsTwo").hide();
       $(".resultsThree").hide();
       } 
+
       event.preventDefault();
 
-      if (firstAnswer === 'numbers' || secondAnswer === 'style' || thirdAnswer === 'old' || fourthAnswer === 'run' || fifthAnswer === 'mornings') {
+      if (firstAnswer === 'numbers' && secondAnswer === 'style') {
         $(".resultsOne").show();
         $(".resultsTwo").hide();
         $(".resultsThree").hide();
         $("#invalidEntry").hide();
         
-      } else if (firstAnswer === 'letters' || secondAnswer === 'function' || thirdAnswer === 'new' || fourthAnswer === 'walk' || fifthAnswer === 'evenings') {
+      } else if (firstAnswer === 'numbers' && thirdAnswer === 'old') {
+        $(".resultsOne").show();
+        $(".resultsTwo").hide();
+        $(".resultsThree").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'numbers' && fourthAnswer === 'sit') {
+        $(".resultsOne").show();
+        $(".resultsTwo").hide();
+        $(".resultsThree").hide();
+        $("#invalidEntry").hide();
+        
+      } else if (firstAnswer === 'numbers' && fifthAnswer === 'mornings'){
+        $(".resultsOne").show();
+        $(".resultsTwo").hide();
+        $(".resultsThree").hide();
+        $("#invalidEntry").hide();
+      }
+
+      event.preventDefault();
+      
+      if (firstAnswer === 'letters' && secondAnswer === 'function') {
         $(".resultsTwo").show();
         $(".resultsOne").hide();
         $(".resultsThree").hide();
         $("#invalidEntry").hide();
-      } else if (firstAnswer === 'symbols' || secondAnswer === 'complex' || thirdAnswer === 'tv' || fourthAnswer === 'sit' || fifthAnswer === 'twilight') {
-        $(".resultsThree").show();
+
+      } else if (firstAnswer === 'letters' && thirdAnswer === 'tv') {
+        $(".resultsTwo").show();
+        $(".resultsThree").hide();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'letters' && fourthAnswer === 'walk'){
+        $(".resultsTwo").show();
+        $(".resultsThree").hide();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'letters' && fifthAnswer === 'twilight'){
+        $(".resultsTwo").show();
+        $(".resultsThree").hide();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
+      }
+      
+      event.preventDefault();
+
+      if (firstAnswer === 'symbols' && secondAnswer === 'complex') {
+        $(".resultsTwo").show();
+        $(".resultsOne").hide();
+        $(".resultsThree").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'symbols' && thirdAnswer === 'new') {
+        $(".resultsTwo").show();
         $(".resultsTwo").hide();
         $(".resultsOne").hide();
         $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'symbols' && fourthAnswer === 'walk'){
+        $(".resultsTwo").show();
+        $(".resultsTwo").hide();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'symbols' && fifthAnswer === 'mornings'){
+        $(".resultsTwo").show();
+        $(".resultsTwo").hide();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
       }
    
 
