d27e9f2b1c270a7ebe136e02a1ba8e14f120528a
Ultra Zacky <ultrazacky@gmail.com>
2022-01-14 09:29:13 -0800

    add javascript for submit button and variables to store user answers

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $("form#selections").submit(function(){
+
+    const firstAnswer = $("select#numLet").val(); 
+    const secondAnswer = $("select#stylFunc").val();
+    const thirdAnswer = $("select#oldNew").val();
+    const fourthAnswer = $("select#runWalk").val();
+    const fifthAnswer = $("select#mornEven").val();
+
+  });
+
+});
