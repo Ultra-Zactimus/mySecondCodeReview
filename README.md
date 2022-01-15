7ef519c49a69fee3848ff501a5c456b4230412bc
Ultra Zacky <ultrazacky@gmail.com>
2022-01-14 16:56:12 -0800

    add README with info on document and listing all errors with document in it's current state.
    Authored-By: Zachary Green <ultrazacky@gmail.com>

---
+++ README.md
@@ -0,0 +1,38 @@
+# My Second Code Review
+
+
+Authored by: Zachary Green
+
+Under the tutorship of Epicodus
+#
+
+This document is a Work-in-Progress. It contains all the key elements of coding in HTML including JavaScript. Document is a quiz to determine which programming launguage the user should study. This answer is based on 5 questions which 2 choices in each.
+#
+
+Link to Document:
+#
+https://github.com/Ultra-Zactimus/mySecondCodeReview
+#
+
+Technologies Used: HTML, CSS, Markdown, GitBash, gitHub
+#
+
+Application Setup Instructions
+#
+
+Click on link in ReadMe.
+
+Click to open the HTML file
+#
+
+Link to User Page: https://github.com/Ultra-Zactimus
+#
+
+Known Bugs:
+#
+Picking different options doesnt display all 3 options. Only Ruby on Rails result is displying at this time. Will be needing to repair these issues.
+#
+
+Copyright of Zachary Green
+January 14th 2022
+This is an Open Source document licenced with the MIT Open Source Licence
--- js/scripts.js
+++ js/scripts.js
@@ -39,37 +39,82 @@ $(document).ready(function() {
         $(".resultsOne").show();
         $(".resultsTwo").hide();
         $(".resultsThree").hide();
+        $("#invalidEntry").hide()
+
+      } else if (firstAnswer === 'numbers' && secondAnswer === 'function') {
+        $(".resultsTwo").hide();
+        $(".resultsOne").show();
+        $(".resultsThree").hide();
         $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'numbers' && thirdAnswer === 'new') {
+        $(".resultsTwo").hide();
+        $(".resultsTwo").hide();
+        $(".resultsOne").show();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'numbers' && fourthAnswer === 'walk'){
+        $(".resultsTwo").show();
+        $(".resultsTwo").hide();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'numbers' && fifthAnswer === 'evenings'){
+        $(".resultsTwo").hide();
+        $(".resultsTwo").show();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'letters' && fifthAnswer === 'style'){
+        $(".resultsTwo").hide();
+        $(".resultsTwo").hide();
+        $(".resultsOne").show();
+        $("#invalidEntry").hide();
+      
+      }else if (firstAnswer === 'letters' && fifthAnswer === 'old'){
+        $(".resultsTwo").show();
+        $(".resultsTwo").hide();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'letters' && fifthAnswer === 'run'){
+        $(".resultsTwo").hide();
+        $(".resultsTwo").show();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'letters' && fifthAnswer === 'mornings'){
+        $(".resultsTwo").hide();
+        $(".resultsTwo").hide();
+        $(".resultsOne").show();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'letters' && fifthAnswer === 'function'){
+        $(".resultsTwo").show();
+        $(".resultsTwo").hide();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+     
+      } else if (firstAnswer === 'letters' && fifthAnswer === 'new'){
+        $(".resultsTwo").hide();
+        $(".resultsTwo").show();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'letters' && fifthAnswer === 'walk'){
+        $(".resultsTwo").hide();
+        $(".resultsTwo").hide();
+        $(".resultsOne").show();
+        $("#invalidEntry").hide();
+
+      } else if (firstAnswer === 'letters' && fifthAnswer === 'evenings'){
+        $(".resultsTwo").show();
+        $(".resultsTwo").hide();
+        $(".resultsOne").hide();
+        $("#invalidEntry").hide();
+
       }
       
-      event.preventDefault();
-      
-        if (firstAnswer === 'letters' && secondAnswer === 'function') {
-          $(".resultsTwo").show();
-          $(".resultsOne").hide();
-          $(".resultsThree").hide();
-          $("#invalidEntry").hide();
-
-        } else if (firstAnswer === 'letters' && thirdAnswer === 'tv') {
-          $(".resultsTwo").hide();
-          $(".resultsTwo").show();
-          $(".resultsOne").hide();
-          $("#invalidEntry").hide();
-
-        } else if (firstAnswer === 'letters' && fourthAnswer === 'walk'){
-          $(".resultsTwo").hide();
-          $(".resultsTwo").hide();
-          $(".resultsOne").show();
-          $("#invalidEntry").hide();
-
-        } else if (firstAnswer === 'letters' && fifthAnswer === 'evenings'){
-          $(".resultsTwo").show();
-          $(".resultsTwo").hide();
-          $(".resultsOne").hide();
-          $("#invalidEntry").hide();
-
-        }
-        
     event.preventDefault();
 
   });
